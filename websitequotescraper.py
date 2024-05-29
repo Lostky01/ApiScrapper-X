deebd021b4e26abb4d960ad673c8529bd7336e83
rifky <106081618+Lostky01@users.noreply.github.com>
2024-05-29 13:45:02 +0700

    dont download it bad code youll get mad

--- websitequotescraper.py
+++ websitequotescraper.py
@@ -1,9 +1,6 @@
 # -*- coding: utf-8 -*-
-"""WebsiteQuoteScraper.ipynb
+"""WebsiteQuoteScraper.py
 """
-
-!pip install requests beautifulsoup4 pandas
-
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
